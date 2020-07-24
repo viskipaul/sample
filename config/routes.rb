1c6d08bbdbd6e4c50e0cdbfcbca403d6ab9cb3da
viskipaul <viskipaul@gmail.com>
2020-07-24 18:01:36 +0300

    Finish logout

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  get 'logout'  => 'sessions#destroy'
 end
