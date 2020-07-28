bc29d6080facf87a2a6ee4cf96e71bcdf8b96f80
viskipaul <viskipaul@gmail.com>
2020-07-28 16:09:48 +0300

    Add image upload/finish microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,4 +37,8 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   get 'logout'  => 'sessions#destroy'
+  resources :microposts,          only: [:create, :destroy]
 end
